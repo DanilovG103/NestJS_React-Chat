142147ef70ff0c6fb86c5377a3d59b2a0ac41c50
DanilovG103 <DanilovG103>
2021-07-06 16:53:00 +0400

    u

---
+++ server/src/chat/message.repository.ts
@@ -0,0 +1,18 @@
+import { Message} from "./message.entity";
+import { Repository, EntityRepository } from "typeorm";
+
+@EntityRepository(Message)
+export class MessageRepository extends Repository<Message> {
+  async createMessage(data: any): Promise<any> {
+    const { author, content } = data;
+
+    const message = this.create();
+
+    message.content = content;
+    message.author = author;
+
+    await this.save(message);
+
+    return message;
+  }
+}
