0aa43ab0ccd7fa9e9d0d3ca054983b5665cb817d
DanilovG103 <80414793+DanilovG103@users.noreply.github.com>
2021-07-08 18:02:44 +0400

    fixed:createUser func is located at auth serv now

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,13 +1,11 @@
-import { Body, Controller, Get, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
-import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { Controller, Get} from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from '../dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
 
     constructor(private usersService: UsersService){}
-    
+
     @Get()
     async getAll(){
         return await this.usersService.getAllUsers()
