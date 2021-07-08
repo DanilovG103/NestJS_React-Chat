bd3ddd9e18101e4f94fe1af8ccd8ec0c826b2c1d
DanilovG103 <80414793+DanilovG103@users.noreply.github.com>
2021-07-08 18:03:51 +0400

    fixed: changed createUser func to use in auth serv

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -4,19 +4,29 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcryptjs'
 import { Users } from './users.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UsersService {
-    constructor(@InjectRepository(Users) private userRepository: Repository<Users>){}
+    constructor(@InjectRepository(Users) private userRepository: Repository<Users>,
+    private jwtService: JwtService){}
 
     async createUser(userDto: CreateUserDto) {
         const newUser = await this.getUserByLogin(userDto.login)
         const hashPassword = await bcrypt.hash(userDto.password, 5);
-        if (!newUser) {
-          const createdUser = this.userRepository.create({ ...userDto, password: hashPassword });
-          return await this.userRepository.save(createdUser);
+
+        if (newUser) throw new BadRequestException('Пользователь с таким логином уже есть') 
+        
+        const createdUser = this.userRepository.create({ ...userDto, password: hashPassword });
+        await this.userRepository.save(createdUser);  
+        return await this.generateToken(createdUser) 
+    }
+
+    private async generateToken(user: Users) {
+        const payload = {login: user.login,id: user.id}
+        return {
+            token: this.jwtService.sign(payload)
         }
-        throw new BadRequestException('Пользователь с таким логином уже есть') 
     }
 
     async getUserByLogin(login: string) {
