fe00183f5e292c2e5b07bc6a2a5b23d34913424f
DanilovG103 <80414793+DanilovG103@users.noreply.github.com>
2021-07-09 13:12:27 +0400

    fixed: authRoutes now work with Cookies

--- client/src/components/AppRouter.tsx
+++ client/src/components/AppRouter.tsx
@@ -1,10 +1,12 @@
+import React from 'react'
+import Cookies from 'js-cookie'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import { authRoutes, routes } from './routes'
 
 const AppRouter = () => {   
     return (
         <Switch>
-            {(localStorage.getItem('token')) && authRoutes.map(({path, component}) => 
+            {(Cookies.get('token')) && authRoutes.map(({path, component}) => 
                 <Route key={path} path={path} component={component} exact/>
             )}
             {routes.map(({path, component}) => 
