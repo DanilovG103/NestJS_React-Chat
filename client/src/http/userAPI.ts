13f83f54b9a0c28956f05558f5587577a5cffaf8
DanilovG103 <80414793+DanilovG103@users.noreply.github.com>
2021-07-09 13:10:57 +0400

    fixed: now used Cookie instead localStorage

--- client/src/http/userAPI.ts
+++ client/src/http/userAPI.ts
@@ -1,14 +1,15 @@
 import jwtDecode from "jwt-decode"
 import {$host} from "./index"
+import Cookies from 'js-cookie'
 
 export const registration = async (login:string, password:string) => {
     const {data} = await $host.post('/auth/registration', {login, password})
-    localStorage.setItem('token', data.token)
+    Cookies.set('token', data.token, {secure: true})
     return jwtDecode(data.token)
 }
 
 export const login = async (login:string, password:string) => {
     const {data} = await $host.post('/auth/login', {login, password})
-    localStorage.setItem('token', data.token)
+    Cookies.set('token', data.token, {secure: true})
     return jwtDecode(data.token)
 }
