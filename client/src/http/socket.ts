6cd88372c7ede05ea9b2910157b4ad888261b8ad
DanilovG103 <80414793+DanilovG103@users.noreply.github.com>
2021-07-09 13:11:30 +0400

    fixed: removed empty constructor

--- client/src/http/socket.ts
+++ client/src/http/socket.ts
@@ -3,8 +3,6 @@ import socketIOClient from "socket.io-client"
 export class Socket {
   private static instance : any
 
-  private constructor() {}
-
   public static getInstance(): any {
     if (!Socket.instance) {
       Socket.instance = socketIOClient('http://localhost:8081',{reconnection: true,transports: ["websocket"]})
